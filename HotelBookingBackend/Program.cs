9c5195c32e91e99e334ca5611fdc1df883a9aea4
pnitram <martin.pedersen@me.com>
2017-03-25 14:00:57 +0100

    Fikset på navn v3

--- HotelBookingBackend/Program.cs
+++ HotelBookingBackend/Program.cs
@@ -16,7 +16,7 @@ namespace HotellBookWF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HotellBookForm());
+            Application.Run(new HotelBookForm());
         }
     }
 }
